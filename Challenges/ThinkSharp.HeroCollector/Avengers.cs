57955abf5f406833d93753e8a1bd7d090d53e273
Kimleng <kimleng-lim@outlook.com>
2025-05-30 13:22:28 -0400

    Add hero collector project

---
+++ Challenges/ThinkSharp.HeroCollector/Avengers.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+
+public class Avengers : IEnumerator<string>
+{
+    public string Current { get; private set; } = "";
+
+    object IEnumerator.Current => Current;
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        return new Avengers();
+    }
+
+    private void SetString0()
+    {
+        Current = "Thor";
+    }
+
+    private void SetString1()
+    {
+        Current = "Iron Man";
+    }
+
+    private void SetString2()
+    {
+        Current = "Hulk";
+    }
+
+    private int _step = 0;
+    public bool MoveNext()
+    {
+        switch (_step)
+        {
+            case 0:
+                SetString0();
+                ++_step;
+                break;
+            case 1:
+                SetString1();
+                ++_step;
+                break;
+            case 2:
+                SetString2();
+                ++_step;
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    public void Reset()
+    {
+        Current = "";
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Challenges/ThinkSharp.HeroCollector/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hero Collector");
+Console.WriteLine("---------------------");
+
+IEnumerable<string> GetHeros()
+{
+    var list = new List<string>();
+    list.Add("Thor");
+    list.Add("Iron Man");
+    list.Add("Hulk");
+    return list;
+}
+foreach (var hero in GetHeros())
+{
+    Console.WriteLine($"Get Hero: {hero}");
+}
+
+// With yield return
+Console.WriteLine("Get Hero with yield return");
+Console.WriteLine("---------------------------");
+IEnumerable<string> GetAvengers()
+{
+    yield return "Thor";
+    yield return "Iron Man";
+    yield return "Hulk";
+}
+
+foreach (var hero in GetAvengers())
+{
+    Console.WriteLine($"Get Hero: {hero}");
+}
+
+// Get Hero with .NET runtime implemention
+Console.WriteLine("Get Hero with .NET runtime implemention");
+Console.WriteLine("-----------------------------------------");
+foreach (var hero in new Avengers()) {
+    Console.WriteLine($"Get hero: {hero}");
+}
